cf3dc8be45264acdd6f87298e4a156880807a0fd
Raju Kumar <129931026+Rajusudha@users.noreply.github.com>
2023-12-19 16:00:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+  // Add an event listener for the DOMContentLoaded event
+    document.addEventListener('DOMContentLoaded', function() {
+      // Code to be executed when the DOM is fully loaded
+      var messageParagraph = document.createElement('p');
+      messageParagraph.textContent = 'DOM load success';
+      document.body.innerHTML = ''; // Clear the body content
+      document.body.appendChild(messageParagraph);
+    });
